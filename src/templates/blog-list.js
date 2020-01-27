46fb5b18d6056d70fdc6ea63ccdd2e1031fc9c57
Wai Min <leopard23@gmail.com>
2020-01-27 22:11:17 +0800

    Fix linking from post list

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -23,31 +23,33 @@ class BlogIndex extends React.Component {
         />
         {posts.map(({ node }) => {
           return (
-            <article className="post" key={node.fields.slug}>
-              {node.frontmatter.img &&
-                node.frontmatter.img.childImageSharp &&
-                node.frontmatter.img.childImageSharp.fluid && (
-                  <Link
-                    to={node.fields.slug}
-                    className="post-thumbnail"
-                    style={{
-                      backgroundImage: `url(${node.frontmatter.img.childImageSharp.fluid.src})`,
-                    }}
-                  />
-                )}
-              <div className="post-content">
-                <h2 className="post-title">
-                  <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-                </h2>
-                <p>{node.excerpt}</p>
-                <span className="post-date">
-                  {node.frontmatter.date}&nbsp;&nbsp;—&nbsp;
-                </span>
-                <span className="post-words">
-                  {node.timeToRead} minute read
-                </span>
-              </div>
-            </article>
+            <Link className="post-link" to={node.fields.slug}>
+              <article className="post" key={node.fields.slug}>
+                {node.frontmatter.img &&
+                  node.frontmatter.img.childImageSharp &&
+                  node.frontmatter.img.childImageSharp.fluid && (
+                    <Link
+                      to={node.fields.slug}
+                      className="post-thumbnail"
+                      style={{
+                        backgroundImage: `url(${node.frontmatter.img.childImageSharp.fluid.src})`,
+                      }}
+                    />
+                  )}
+                <div className="post-content">
+                  <h2 className="post-title">
+                    <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+                  </h2>
+                  <p>{node.excerpt}</p>
+                  <span className="post-date">
+                    {node.frontmatter.date}&nbsp;&nbsp;—&nbsp;
+                  </span>
+                  <span className="post-words">
+                    {node.timeToRead} minute read
+                  </span>
+                </div>
+              </article>
+            </Link>
           )
         })}
 
